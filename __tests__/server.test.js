6db72e30683c4668ccca31e995fa99419c4af2e8
Ibrahim Abu-awad <ibrahimabuawadwork@gmail.com>
2021-06-30 20:21:01 +0300

    done

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -9,11 +9,11 @@ describe('Server Test Group', () => {
     expect(response.status).toEqual(404);
   });
   it('Handles bad method', async () => {
-    const response = await request.post('/person?name=amro');
+    const response = await request.post('/person?name=ibrahim');
     expect(response.status).toEqual(404);
   });
   it('Handles creating new food', async () => {
-    let foodObj = { name: 'test', price: 'test' };
+    let foodObj = { name: 'test', price: '3' };
     const response = await request.post('/api/v1/food').send(foodObj);
     id = response.body._id;
     expect(response.body.name).toBe(foodObj.name);
@@ -23,22 +23,22 @@ describe('Server Test Group', () => {
   it('Handles reading foods', async () => {
     const response = await request.get('/api/v1/food');
     expect(response.body[0].name).toBe('test');
-    expect(response.body[0].price).toBe('test');
+    expect(response.body[0].price).toBe('3');
     expect(response.body.length).toBe(1);
     expect(response.status).toEqual(200);
   });
   it('Handles updating a record', async () => {
     const newObj = {
-      name: 'potato',
+      name: 'NO',
       price: '5',
     };
     const response = await request.put('/api/v1/food/' + id).send(newObj);
     expect(response.status).toEqual(200);
-    expect(response.body.name).toBe('potato');
+    expect(response.body.name).toBe('NO');
   });
   it('Handles deleting a record', async () => {
     const response = await request.delete('/api/v1/food/' + id);
     expect(response.status).toEqual(200);
-    expect(response.body.name).toBe('potato');
+    expect(response.body.name).toBe('NO');
   });
 });
